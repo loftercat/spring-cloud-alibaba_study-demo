4a098230673234179be5a4bee1143783f24693c5
lofter <723202177@qq.com>
2026-06-13 21:43:51 +0800

    feat(order): 配置OpenFeign拦截器

--- services/service-order/src/main/java/com/su/order/config/OrderConfig.java
+++ services/service-order/src/main/java/com/su/order/config/OrderConfig.java
@@ -1,15 +1,25 @@
 package com.su.order.config;
 
 import feign.Logger;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
 import feign.Retryer;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.UUID;
+
 @Configuration
 public class OrderConfig {
 
+    // 配置feign的请求拦截器，为每个请求添加X-Request-Id头
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        return (RequestTemplate request) -> request.header("X-Request-Id", UUID.randomUUID().toString());
+    }
+    
     // 配置feign的重试策略，默认5次
     @Bean
     public Retryer retryer() {
--- services/service-product/src/main/java/com/su/product/controller/ProductController.java
+++ services/service-product/src/main/java/com/su/product/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.su.product.controller;
 
 import com.su.product.bean.Product;
 import com.su.product.service.impl.ProductService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,8 +18,10 @@ public class ProductController {
     private final ProductService productService;
 
     @RequestMapping("/{id}")
-    public Product getProduct(@PathVariable("id") Long productId) throws InterruptedException {
+    public Product getProduct(@PathVariable("id") Long productId, HttpServletRequest request) throws InterruptedException {
         log.info("getProduct被调用: {}", productId);
+        request.getHeader("X-Request-Id");
+        log.info("X-Request-Id: {}", request.getHeader("X-Request-Id"));
         return productService.getProductById(productId);
     }
 }
