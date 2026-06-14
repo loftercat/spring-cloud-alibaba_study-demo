d7cbee66a2eeca32983d47ddb027c16de6a86c76
lofter <723202177@qq.com>
2026-06-14 15:58:35 +0800

    feat(order): Sentinel流控规则： 流控模式：关联测试

--- services/service-order/src/main/java/com/su/order/controller/OrderController.java
+++ services/service-order/src/main/java/com/su/order/controller/OrderController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/order")
+@RequestMapping
 public class OrderController {
 
     private final OrderService orderService;
@@ -47,5 +48,14 @@ public class OrderController {
         return "timeout: " + timeout + ", autoConfirm: " + autoConfirm + ", url: " + url;
     }
 
+    @GetMapping("/writeDb")
+    public String writeBd() {
+        return "writeBd";
+    }
+
+    @GetMapping("/readDb")
+    public String readBd() {
+        return "readBd";
+    }
 
 }
